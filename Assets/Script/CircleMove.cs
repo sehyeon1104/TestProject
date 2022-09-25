86fa4b9ee9b8329b4ffa6398e559149547546001
sehyeon1104 <sehyeon1104@naver.com>
2022-09-25 23:53:15 +0900

    push

--- Assets/Script/CircleMove.cs
+++ Assets/Script/CircleMove.cs
@@ -4,29 +4,10 @@ using UnityEngine;
 
 public class CircleMove : MonoBehaviour
 {
-    public GameObject[] obj;
-    float circleR;
-    float deg;
-    float objSpeed;
+   
 
     void Update()
     {
-        deg += Time.deltaTime * objSpeed;
-        if (deg < 360)
-        {
-            for (int i = 0; i < obj.Length; i++)
-            {
-                var rad = Mathf.Deg2Rad * (deg + (i * (360 / obj.Length)));
-                var x = circleR * Mathf.Sin(rad);
-                var y = circleR * Mathf.Cos(rad);
-                obj[i].transform.position = transform.position + new Vector3(x, y);
-                obj[i].transform.rotation = Quaternion.Euler(0, 0, (deg + (i * (360 / obj.Length))) * -1);
-            }
-
-        }
-        else
-        {
-            deg = 0;
-        }
+       
     }
 }
