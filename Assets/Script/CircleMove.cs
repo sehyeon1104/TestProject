d546d62e5f44b4b2cfa140a36ed86a7ee340f0d7
sehyeon1104 <sehyeon1104@naver.com>
2022-09-26 13:59:49 +0900

    mathf.pi

--- Assets/Script/CircleMove.cs
+++ Assets/Script/CircleMove.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class CircleMove : MonoBehaviour
 {
-   
+   Transform tran;
+    Vector3 retVector;
 
+    public float speed = 1;
+    private float degree = 0;
+    float radian = 0;
+    private void Awake()
+    {
+        tran= GetComponent<Transform>();
+    }
     void Update()
     {
-       
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            degree += speed;
+             radian = degree * Mathf.PI / 180; //라디안값
+
+            retVector.x += 1 * Mathf.Cos(radian);
+            retVector.y += 1 * Mathf.Sin(radian);
+
+            tran.position = retVector;
+        }
     }
 }
