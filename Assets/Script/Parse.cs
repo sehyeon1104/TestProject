143791a4380c606a9d5eb3c36f3e31563821cc7f
sehyeon1104 <sehyeon1104@naver.com>
2022-09-29 11:17:35 +0900

    parse
    string 을 int로 형변환활때 int.parse(string 변수를 하면 형변환이 된)

---
+++ Assets/Script/Parse.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parse : MonoBehaviour
+{
+    string ss = "109";
+    int a = 0;
+    private void Start()
+    {
+      print(int.Parse(ss));
+    }
+}
