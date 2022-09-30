ee09fb6f7788e107f366d83e5b0b687400185e48
sehyeon1104 <sehyeon1104@naver.com>
2022-09-30 09:23:08 +0900

    case 식문 배움

--- Assets/Script/Parse.cs
+++ Assets/Script/Parse.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class Parse : MonoBehaviour
 {
-    string ss = "109";
+    string ss = "90";
     int a = 0;
+    bool us = true;
     private void Start()
     {
-      print(int.Parse(ss));
+        a = Convert.ToInt32(ss);
+    }
+    private void Update()
+    {
+        int aa = a switch
+        {
+            10 => 1,
+            30 =>3,
+            _  when us==true=> 2, 
+            _=>4
+        };
+        print(aa);
+       
     }
 }
