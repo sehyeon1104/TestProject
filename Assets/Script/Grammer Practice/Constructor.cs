1b7afbbb05b539ee4fe2a8d67bbdddcfd5d35086
sehyeon1104 <sehyeon1104@naver.com>
2022-10-21 11:16:26 +0900

    PUSH

---
+++ Assets/Script/Grammer Practice/Constructor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constructor : MonoBehaviour
+{
+    Cat sehyeon = new Cat("세현", "18");
+    Cat minyeong = new Cat("민영", "17");
+    void Start()
+    {
+        sehyeon.Ache();
+        minyeong.Ache();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+class Cat : MonoBehaviour
+    {
+    string name;
+    string age;
+  public Cat(string _name, string _age)
+    {
+        name = _name;
+        age = _age; 
+    }
+    ~Cat()
+    {
+        print($"{name} 잘가요");
+    }
+    public void Ache()
+    {
+        print($"고양이의 이름은 {name} 이고 나이는 {age}이다");
+    }
+}
