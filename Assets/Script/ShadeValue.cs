caf7ad9edf050cc5d699ed92161e3384b6d60d5c
sehyeon1104 <sehyeon1104@naver.com>
2022-10-27 19:57:31 +0900

    push

---
+++ Assets/Script/ShadeValue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeValue : MonoBehaviour
+{
+    [SerializeField]
+    Renderer m_Renderer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
