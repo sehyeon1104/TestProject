1823864af36d20e729dcc6d1f84782205cbbf786
sehyeon1104 <sehyeon1104@naver.com>
2022-11-23 08:38:43 +0900

    push

--- Assets/01.Script/FOV.cs
+++ Assets/01.Script/FOV.cs
@@ -9,17 +9,14 @@ public class FOV : MonoBehaviour
     [SerializeField]
     [Tooltip("시야각")]
     float angle = 90f;
-    Vector3 LookTarget=new Vector3(0,0,0);
+    
+    
     void Start()
     {
 
     }
     void Update()
     {
-     LookTarget = (target.position - transform.position).normalized;
-        if(Vector3.Angle(transform.forward,LookTarget)<angle/2)
-        {
-            print("적 발견");
-        }
+    Vector3 dot =Vector3.Dot(transform.forward)
     }
 }
